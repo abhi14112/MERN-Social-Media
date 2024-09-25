903cfaf184fae3599eba3395df16aa90dbd3702f
Abhishek kumar <aabhi9478@gmail.com>
2024-09-25 12:02:20 +0530

    cors

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: 'http://localhost:5000',credentials:true }));
+app.use(cors());
 const PORT = process.env.PORT;
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
